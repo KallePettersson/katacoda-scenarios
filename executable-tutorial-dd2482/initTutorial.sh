547ba548f167de08cfe530d5dd858131590c3932
Kalle Pettersson <kalpet@kth.se>
2021-04-21 20:58:35 +0200

    test

--- executable-tutorial-dd2482/initTutorial.sh
+++ executable-tutorial-dd2482/initTutorial.sh
@@ -2,7 +2,7 @@
 apt-get install -y cxxtest
 
 # Clone git repo
-git clone https://github.com/KallePettersson/devops-executable-tutorial.git 
+git clone -b tutorial-start https://github.com/KallePettersson/devops-executable-tutorial.git 
 
 
 # Set up tutorial-start branch to correct commit
