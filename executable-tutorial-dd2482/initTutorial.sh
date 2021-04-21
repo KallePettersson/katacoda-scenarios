3dc69faae91cefbd812229e335b7c449e51f2bdf
Kalle Pettersson <kalpet@kth.se>
2021-04-21 11:14:02 +0200

    test

--- executable-tutorial-dd2482/initTutorial.sh
+++ executable-tutorial-dd2482/initTutorial.sh
@@ -1,6 +1,3 @@
 sudo apt-get update -y
 sudo apt-get install -y cxxtest
-git clone https://github.com/KallePettersson/devops-executable-tutorial.git
-cd devops-executable-tutorial
-git checkout tutorial-start
 
