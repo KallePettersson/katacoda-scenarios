a9f5d998c4b207d42081e7236919f9da76d694e5
Kalle Pettersson <kalpet@kth.se>
2021-04-21 21:03:08 +0200

    fixat init scriptet

--- executable-tutorial-dd2482/initTutorial.sh
+++ executable-tutorial-dd2482/initTutorial.sh
@@ -1,14 +1,13 @@
 # Install CxxTest
+apt-get update -y
 apt-get install -y cxxtest
 
 # Clone git repo
 git clone -b tutorial-start https://github.com/KallePettersson/devops-executable-tutorial.git 
 
-
-# Set up tutorial-start branch to correct commit
-
-# git reset --hard <commit>
-
-
 # Move to right repo
 cd devops-executable-tutorial
+
+# Set up tutorial-start branch to correct commit
+# git reset --hard <commit>
+
