47ca73aa2ffd7dac0bf49a0bd27bbeee89fb0e24
Kalle Pettersson <kalpet@kth.se>
2021-05-25 21:41:07 +0200

    repeat version of tutorial

---
+++ repeat-executable-tutorial/cleanUp.sh
@@ -0,0 +1,7 @@
+# Revert all branches to the correct state in case someone fucked with it
+
+# Move to right repo
+cd devops-executable-tutorial
+
+# Set up tutorial-start branch to correct commit
+# git reset --hard <commit>
---
+++ repeat-executable-tutorial/initTutorial.sh
@@ -0,0 +1,13 @@
+# Install CxxTest
+apt-get update -y
+apt-get install -y cxxtest
+
+# Clone git repo
+git clone -b tutorial-start https://github.com/KallePettersson/devops-executable-tutorial.git 
+
+# Move to right repo
+cd devops-executable-tutorial
+
+# Set up tutorial-start branch to correct commit
+# git reset --hard <commit>
+
